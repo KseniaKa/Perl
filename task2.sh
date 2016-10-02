e38a5f49d6af6296e4de1be8326824e488b518aa
Ксения Кузнецова <ksusha20010@yandex.ru>
2016-10-03 00:35:38 +0400

    Lecture 1

--- task2.sh
+++ task2.sh
@@ -1,1 +1,1 @@
-cat file.txt | perl -l -e 'my $count1=0; my $count2=0; while(<>){chomp; my @f=split(/;/,$_, 0); $count1++; if($f[4]>2**20){print $f[scalar @f-1]; $count2++}};print $count1; print $count2'
+cat file.txt | perl -lnaF';' -e '{if($F[4]>2**20){print $F[scalar @F-1]; $count++}}; if(eof){print $.; print $count}'
